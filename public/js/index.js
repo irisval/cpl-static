ab91d30ee8dad5c2f885088f80cee2e18fc9de50
irisval <iris.val19@gmail.com>
2020-07-08 15:34:36 -0400

    Homepage basic responsiveness.

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,25 @@
 $(document).ready(function(){
+
+  $('.burger').click(function() {
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
+
   $('.home-news-slider').slick({
     infinite: true,
     slidesToShow: 1,
     variableWidth: true,
     prevArrow: $('#left'),
     nextArrow: $('#right'),
-    draggable: true
+    draggable: true,
+    // responsive: [
+    // {
+    //   breakpoint: 768,
+    //   settings: {
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1
+    //   }
+    // }
+    // ]
   });
 });
